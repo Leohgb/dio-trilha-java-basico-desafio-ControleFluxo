07eb09570f89daa9b46bf47037440c19a1331eaf
Leohgb <leonardokgb12@gmail.com>
2024-06-01 23:54:34 -0300

    Primeiro commit

---
+++ src/main/java/com/mycompany/desafiocontrolefluxo/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+package com.mycompany.desafiocontrolefluxo;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem){
+        super(mensagem);
+    }
+    
+}
